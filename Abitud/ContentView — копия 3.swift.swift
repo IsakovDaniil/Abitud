263ee96703736e2fb4061503b6f8ac18639b93f4
IsakovDaniil <serverpgig@yandex.ru>
2026-06-15 21:47:48 +0300

    1

---
+++ Abitud/ContentView — копия 3.swift.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Abitud
+//
+//  Created by Даниил on 05.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                    } label: {
+                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                    
+                }
+                .onDelete(perform: deleteItems)
+                
+                Text("Okey Bro")
+                    .font(.system(size: 13))
+                    .foregroundStyle(.appRed)
+                    .padding(3)
+                    .lineLimit(4)
+            }
+            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
+            .toolbar {
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        } detail: {
+            Text("Select an item")
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+            let beg = 1
+        }
+    }
+    
+
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
